f5218be088d8f7ea85887cb7cbac4d4c52dad379
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-10-13 23:29:33 +0600

    basic setup for coffee shop client

---
+++ src/pages/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Root = () => {
+    return (
+        <div>
+            root
+            <Outlet />
+        </div>
+    );
+};
+
+export default Root;
